eb3c86858ce6266d4e0ae3c5288fa2e142206c20
Jeeves <98932839+OPCatGaming@users.noreply.github.com>
2024-01-20 10:58:23 -0500

    Update scoutingPASS.js

--- resources/js/scoutingPASS.js
+++ resources/js/scoutingPASS.js
@@ -870,20 +870,20 @@ function getData(dataFormat) {
 }
 
 function updateQRHeader() {
-  let str = 'Event: !EVENT! Match: !MATCH! Robot: !ROBOT! Team: !TEAM!';
+  // let str = 'Event: !EVENT! Match: !MATCH! Robot: !ROBOT! Team: !TEAM!';
 
-  if (!pitScouting) {
-    str = str
-      .replace('!EVENT!', document.getElementById("input_e").value)
-      .replace('!MATCH!', document.getElementById("input_m").value)
-      .replace('!ROBOT!', document.getElementById("display_r").value)
-      .replace('!TEAM!', document.getElementById("input_t").value);
-  } else {
-    str = 'Pit Scouting - Team !TEAM!'
-      .replace('!TEAM!', document.getElementById("input_t").value);
-  }
+  // if (!pitScouting) {
+  //   str = str
+  //     .replace('!EVENT!', document.getElementById("input_e").value)
+  //     .replace('!MATCH!', document.getElementById("input_m").value)
+  //     .replace('!ROBOT!', document.getElementById("display_r").value)
+  //     .replace('!TEAM!', document.getElementById("input_t").value);
+  // } else {
+  //   str = 'Pit Scouting - Team !TEAM!'
+  //     .replace('!TEAM!', document.getElementById("input_t").value);
+  // }
 
-  document.getElementById("display_qr-info").textContent = str;
+  // document.getElementById("display_qr-info").textContent = str;
 }
 
 
