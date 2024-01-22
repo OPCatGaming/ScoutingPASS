f7bb6d484407ae76f42548cd137a81962e55a380
Jeeves <98932839+OPCatGaming@users.noreply.github.com>
2024-01-22 10:23:18 -0500

    Update TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -2,46 +2,34 @@
 var teams = null;
 var schedule = null;
 var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
-/**
- * Get list of teams in event
- *
- * @param {eventCode} eventCode the event code (i.e. 2020caln) to pull the team list
- */
-function getTeams(eventCode) {
-	if (authKey) {
-		var xmlhttp = new XMLHttpRequest();
-		var url = "https://www.thebluealliance.com/api/v3/event/" + eventCode + "/teams/simple";
-		xmlhttp.open("GET", url, true);
-		xmlhttp.setRequestHeader("X-TBA-Auth-Key", authKey);
-		xmlhttp.onreadystatechange = function() {
-			if (this.readyState == 4 && this.status == 200) {
-				var response = this.responseText;
-				teams = JSON.parse(response);
-			}
-		};
-		// Send request
-		xmlhttp.send();
-	}
+
+function getEvents() {
+	return getAPIDataFromURL("https://www.thebluealliance.com/api/v3/event/simple");
 }
 
-/**
- * Get schefule for event
- *
- * @param {eventCode} eventCode the event code (i.e. 2020caln) to pull the team list
- */
-function getSchedule(eventCode) {
+function getTeams(eventCode) {
+	return getAPIDataFromURL("https://www.thebluealliance.com/api/v3/event/" + eventCode + "/teams/simple");
+}
+
+function getMatches(eventCode) {
+	return getAPIDataFromURL("https://www.thebluealliance.com/api/v3/event/" + eventCode + "/matches/simple");
+}
+
+function getAPIDataFromURL(url) {
+	let parsedData = null;
 	if (authKey) {
 		var xmlhttp = new XMLHttpRequest();
-		var url = "https://www.thebluealliance.com/api/v3/event/" + eventCode + "/matches/simple";
+		var url = url;
 		xmlhttp.open("GET", url, true);
 		xmlhttp.setRequestHeader("X-TBA-Auth-Key", authKey);
 		xmlhttp.onreadystatechange = function() {
 			if (this.readyState == 4 && this.status == 200) {
 				var response = this.responseText;
-				schedule = JSON.parse(response);
+				parsedData = JSON.parse(response);
 			}
 		};
 		// Send request
 		xmlhttp.send();
 	}
+	return parsedData;
 }
